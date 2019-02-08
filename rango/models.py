d154c3a95361a973b87a3cc75bbeedace47afd0e
Liam Brodie <2198998b@student.gla.ac.uk>
2019-02-08 18:25:41 +0000

    Chapter 10; start

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # A model is the single, definitive source of information about your data.
 # It contains the essential fields and behaviors of the data you’re storing.
@@ -36,3 +37,14 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    # Override the __unicode__() method to return out something meaningful!
+    def __str__(self):
+        return self.user.username
