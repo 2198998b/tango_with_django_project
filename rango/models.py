e925eb0998b623123325d13d2cb8ae535a8da834
Liam Brodie <2198998b@student.gla.ac.uk>
2019-02-08 15:43:54 +0000

    Chapter 8; start

--- rango/models.py
+++ rango/models.py
@@ -9,6 +9,7 @@ from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
+    max_name_length = 128
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
@@ -27,9 +28,11 @@ class Category(models.Model):
 
 class Page(models.Model):
     category = models.ForeignKey(Category, models.CASCADE)
-    title = models.CharField(max_length=128)
+    max_title_length = 128
+    title = models.CharField(max_length=max_title_length)
+    max_url_length = 200    # might need to remove, also the reference in forms
     url = models.URLField()
-    views = models.IntegerField(default=0)
+    views = models.IntegerField(default=max_url_length)
 
     def __str__(self):
         return self.title
